cefb24a46c1a19d8e66141b80fdca8ba48a7b00e
NishanthSP <72077212+NishanthSP@users.noreply.github.com>
2021-01-25 15:06:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/codingthunder"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
