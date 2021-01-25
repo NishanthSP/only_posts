828e710bbd11d6662bfd7677ef7e0debe918f4f3
NishanthSP <72077212+NishanthSP@users.noreply.github.com>
2021-01-25 18:22:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,12 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/codingthunder"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
 
 
 
-
-class Posts(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    slug = db.Column(db.String(21), nullable=False)
-    content = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12), nullable=True)
-    link = db.Column(db.String(120), nullable=False)
-    # img_file = db.Column(db.String(12), nullable=True)
-
-
-@app.route("/", methods=['GET'])
+@app.route("/")
 def post_route():
-#     post = Posts.query.filter_by().all()
+    
     return render_template('post.html', post=post)
 
 
