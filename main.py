db396965c4a69c26709c10c750563e9a440bb6de
NishanthSP <72077212+NishanthSP@users.noreply.github.com>
2021-01-25 18:23:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-import json
-from datetime import datetime
-
-
 
 
 app = Flask(__name__)
